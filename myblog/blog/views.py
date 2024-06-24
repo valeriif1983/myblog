0c99c9488440ee800040e8a3c7a9bffdd2a2a5c3
valeron_b <42656663+valeriif1983@users.noreply.github.com>
2024-06-24 20:39:42 +0300

    Add files via upload

---
+++ myblog/blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
